e5a0e70841153338ae6e3c32bcb429660cb1b05c
anupama <anupamagadde@yahoo.com>
2020-04-09 07:38:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your classes here
+
+class Tree {
+    constructor(species) {
+        this.species = species;
+    }
+
+    static definition() {
+        return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
+    }
+}
+
+class Deciduous extends Tree {
+    constructor(species, name) {
+        super(species)
+        this.name = name
+    }
+ //Create a static method, `definition()`, that uses `super` to access
+  //`definition()` from `Tree` and add the following to the provide a specific
+ // definition for `Deciduous`:
+ // Deciduous trees shed their leaves annually.
+    static definition() {
+        return `${super.definition()} Deciduous trees shed their leaves annually.`
+    }
+}
+
+class Evergreen extends Tree {
+    constructor(species, name) {
+        super(species)
+        this.name = name
+    }
+
+    static definition() {
+        return `${super.definition()} Evergreens keep their leaves all year round.`
+    }
+}
